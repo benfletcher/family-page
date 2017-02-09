3d1b941f308383bf1c4e793c06aa8a8cb53250bc
davellaj <jamie.davella@gmail.com>
2017-02-09 02:30:16 -0500

    add component for upload that makes a post to the server to store image url

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { postImg } from '../actions';
 
 class Upload extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: 'Jamie',
+    };
+    this.uploadSubmit = this.uploadSubmit.bind(this);
   }
 
-  urlInput(event) {
+  uploadSubmit(event) {
+    // grabs text url and dispatches action to post to database
     event.preventDefault();
-    // need to create this dispatch function as an async actions
-    this.props.postImg(this.props.userId, this.props.imgDescription);
+    const imageUrl = this.textInput.value;
+    // need to dispatch function as an async actions that takes input og image url
+    // this.state.userId will eventually come from this.props.userId coming from mapStateToProps
+    this.props.postImg({ userId: this.state.userId, url: imageUrl });
+    this.textInput.value = '';
   }
+
   render() {
     return (
       <div>
-        <input
-          type="text"
-          placeholder="url to upload"
-          onChange={this.urlInput}
-        />
+        <form onSubmit={this.uploadSubmit}>
+          <input
+            type="text"
+            placeholder="url to upload"
+            ref={input => this.textInput = input}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default Upload;
+// Upload.defaultProps = {
+//   textInput: 'textInput',
+// };
+
+// Upload.propTypes = {
+//   textInput: React.PropTypes.string,
+//   imgUrl: React.PropTypes.string,
+// };
+
+// eventualy will only get userId from mapStateToProps not the entire state
+const mapStateToProps = state => ({
+  state
+});
+
+const mapDispatchToProps = dispatch => ({
+  postImg: uploadImg => dispatch(postImg(uploadImg))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Upload);
+
+// export default connect(mapDispatchToProps)(Upload);
