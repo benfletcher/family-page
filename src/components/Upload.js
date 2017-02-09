f2e5ea45fe230d8a37230b2a8bd51cca99ab6677
davellaj <jamie.davella@gmail.com>
2017-02-09 16:29:11 -0500

    change variable name imageUrl to photoUrl and add basic validation for input fields to prevent submission on empty fields

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -6,28 +6,32 @@ class Upload extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageUrl: '',
+      photoUrl: '',
       caption: ''
     };
     this.uploadSubmit = this.uploadSubmit.bind(this);
-    this.urlInputChange = this.urlInputChange.bind(this);
+    this.photoUrlInputChange = this.photoUrlInputChange.bind(this);
     this.captionInputChange = this.captionInputChange.bind(this);
   }
 
   uploadSubmit(event) {
     // grabs text url and dispatches action to post to database
     event.preventDefault();
-    this.props.dispatch(actions.postImg({
-      userId: this.props.userId,
-      url: this.state.imageUrl,
-      caption: this.state.caption
-    }));
-    this.setState({ imageUrl: '', caption: '' });
+    // TODO add to input validation
+    if (this.state.photoUrl && this.state.caption) {
+      this.props.dispatch(actions.postImg({
+        userId: this.props.userId,
+        url: this.state.photoUrl,
+        caption: this.state.caption
+      }));
+      this.setState({ photoUrl: '', caption: '' });
+    }
   }
 
-  urlInputChange(event) {
+
+  photoUrlInputChange(event) {
     // console.log(this.state.imageUrl);
-    this.setState({ imageUrl: event.target.value });
+    this.setState({ photoUrl: event.target.value });
   }
 
   captionInputChange(event) {
@@ -38,11 +42,12 @@ class Upload extends Component {
     return (
       <div>
         <form onSubmit={this.uploadSubmit}>
+          <p>Both fields are required at this moment</p>
           <input
             type="text"
             placeholder="url to upload"
-            value={this.state.imageUrl}
-            onChange={this.urlInputChange}
+            value={this.state.photoUrl}
+            onChange={this.photoUrlInputChange}
           />
           <input
             type="text"
