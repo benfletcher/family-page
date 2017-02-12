582defe6dbd1512d63cb26f795765157180402cc
alexpoe22 <alexpoe22@gmail.com>
2017-02-11 16:42:56 -0800

    styling on gallery, plus tests for dumb components

---
+++ src/GalleryThumbnail.test.js
@@ -0,0 +1,12 @@
+/* eslint-env browser, jest */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GalleryThumbnail from './components/GalleryThumbnail';
+
+test('Header snapshot is the same', () => {
+  const component = renderer.create(
+    <GalleryThumbnail />
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/GalleryZoomed.test.js
@@ -0,0 +1,12 @@
+/* eslint-env browser, jest */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GalleryZoomed from './components/GalleryZoomed';
+
+test('Header snapshot is the same', () => {
+  const component = renderer.create(
+    <GalleryZoomed />
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
