1ffc2e953b37e65f637a1d68a231b80a5d09ec44
davellaj <jamie.davella@gmail.com>
2017-02-13 11:45:44 -0500

    clean up linting issues on Upload Component

--- src/components/UploadBox.js
+++ src/components/UploadBox.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dropzone from 'react-dropzone';
 
-class UploadBox extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="FileUpload">
-        <Dropzone
-          multiple={false}
-          accept="image/*"
-          onDrop={this.props.onImageDrop}
-        >
-          <p>Drag and Drop an Image or click to select a file to upload</p>
-        </Dropzone>
-      </div>
+const UploadBox = props => (
+  <div className="FileUpload">
+    <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={props.onImageDrop}
+    >
+      <p>Drag and Drop an Image or click to select a file to upload</p>
+    </Dropzone>
+  </div>
     );
-  }
-}
+
+UploadBox.propTypes = {
+  onImageDrop: React.PropTypes.func.isRequired
+};
 export default UploadBox;
