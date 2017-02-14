0b4105339bdac2f0c23c4d4377f50d2a487cb9f9
alexpoe22 <alexpoe22@gmail.com>
2017-02-14 12:13:04 -0800

    zoomed photo updated

--- src/components/GalleryZoomed.js
+++ src/components/GalleryZoomed.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const GalleryZoomed = props => (
   <div className="galleryZoomed">
-    <img onClick={props.zoom} className="xIcon" src="xIcon.png" alt="close" />
-    <img className="familyPhoto" src={props.photoUrl} alt="large" />
     <div className="zoomedNav">
       <img onClick={props.goLeft} className="arrow" src="arrow.png" alt="go left" />
       <img onClick={props.goRight} className="arrow" src="arrowRight.png" alt="go right" />
     </div>
+    <img onClick={props.zoom} className="xIcon" src="xIcon.png" alt="close" />
+    <img className="familyPhoto" src={props.photoUrl} alt="large" />
   </div>
 );
 
