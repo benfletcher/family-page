f09af19920123567d2a282017e13741260dead94
davellaj <jamie.davella@gmail.com>
2017-02-20 15:08:18 -0500

    working segmented comments

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -3,30 +3,80 @@ import CommentNode from './CommentNode';
 import CommentInput from './CommentInput';
 
 const CommentsContainer = (props) => {
-  const commentBuckets = {
-  };
+  const commentBuckets = {};
 
   props.message.comments.forEach((comment) => {
     // console.log(props.currentUser);
     if (comment.from === props.currentUser) {
       if (!commentBuckets[comment.to]) { commentBuckets[comment.to] = []; }
-      commentBuckets[comment.to].push(<div>{comment.text}</div>);
+      commentBuckets[comment.to].push(
+        <div>
+          <CommentNode
+            comment={comment}
+            messageId={props.message._id}
+            from={
+          comment.from in props.members
+           ? props.members[comment.from].nickname
+           : '...loading...'
+          }
+            fromAvatar={
+          (comment.from in props.members)
+           ? props.members[comment.from].avatar
+           : null
+          }
+            key={comment._id}
+          />
+        </div>);
+    } else {
+      if (!commentBuckets[comment.from]) { commentBuckets[comment.from] = []; }
+      commentBuckets[comment.from].push(<div>
+        <CommentNode
+          comment={comment}
+          messageId={props.message._id}
+          from={
+     comment.from in props.members
+       ? props.members[comment.from].nickname
+       : '...loading...'
+   }
+          fromAvatar={
+     (comment.from in props.members)
+       ? props.members[comment.from].avatar
+       : null
+   }
+          key={comment._id}
+        />
+      </div>);
     }
-
-    if (!commentBuckets[comment.from]) { commentBuckets[comment.from] = []; }
-    commentBuckets[comment.from].push(<div>{comment.text}</div>);
   });
 
-  const CommentsThread = p => (
-    <div>
-      {p.children}
-      <input type="text" />
-    </div>
+  const CommentsThread = (p) => {
+    const replyToName = p.to in p.members
+    ? p.members[p.to].nickname
+    : '...loading...';
+
+    console.log('this is props.children inside commentsThread', p.children);
+    return (
+      <div>
+        {p.children}
+        <CommentInput
+          currentAvatar={p.currentAvatar}
+          messageId={p.messageId}
+          to={p.to}
+          replyToName={replyToName}
+        />
+      </div>
     );
+  };
   const xyz = [];
 
   for (const key in commentBuckets) {
-    xyz.push(<CommentsThread>
+    xyz.push(<CommentsThread
+      to={key}
+      currentAvatar={props.currentAvatar}
+      messageId={props.message._id}
+      messageUserId={props.message.userId}
+      members={props.members}
+    >
       {commentBuckets[key]}
     </CommentsThread>
   );
