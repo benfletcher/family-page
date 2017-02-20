14fdf2e9492828bec1b6bc404c45b2798d06160d
davellaj <jamie.davella@gmail.com>
2017-02-20 13:53:08 -0500

    implemented bucket sort to segment comments then render jsx with input box underneath each

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -3,111 +3,44 @@ import CommentNode from './CommentNode';
 import CommentInput from './CommentInput';
 
 const CommentsContainer = (props) => {
-  if (props.message.userId === props.currentUser) {
-    let sentTo = [];
-    props.message.comments.map((comment) => {
-      if (comment.from !== props.currentUser) {
-        sentTo.push(comment.from);
-      }
-    });
-    sentTo = sentTo.filter((elem, i, array) => array.indexOf(elem) === i);
+  const commentBuckets = {
+  };
 
-    const segment = {};
-    sentTo.map((sender) => {
-      segment[sender] = props.message.comments.filter((comment) => {
-        if (comment.to === sender || comment.from === sender) {
-          return true;
-        }
-      });
-    });
-    console.log('segment ', segment);
-    // now have an object with key of sender's userId and array of comments with that user as value
-    // this object has all the comments of the message
+  props.message.comments.forEach((comment) => {
+    // console.log(props.currentUser);
+    if (comment.from === props.currentUser) {
+      if (!commentBuckets[comment.to]) { commentBuckets[comment.to] = []; }
+      commentBuckets[comment.to].push(<div>{comment.text}</div>);
+    }
 
-    const eachComment = ((segment) => {
-      // console.log('segment inside eachComment', segment);
-      for (const key in segment) {
-        // console.log('segment key/value inside eachComment for loop', key, segment[key]);
-        if (segment.hasOwnProperty(key)) {
-          const commentSegment = segment[key];
-          if (commentSegment.length > 0) {
-            const eachSegment = commentSegment.map(comment =>
-              // console.log('comment inside eachComment', comment);
-               (
-                 <CommentNode
-                   comment={comment}
-                   messageId={props.message._id}
-                   from={
-                comment.from in props.members
-                  ? props.members[comment.from].nickname
-                  : '...loading...'
-              }
-                   fromAvatar={
-                (comment.from in props.members)
-                  ? props.members[comment.from].avatar
-                  : null
-              }
-                   key={comment._id}
-                 />
-              ));
-            const replyToName = key in props.members
-            ? props.members[props.message.userId].nickname
-            : '...loading...';
+    if (!commentBuckets[comment.from]) { commentBuckets[comment.from] = []; }
+    commentBuckets[comment.from].push(<div>{comment.text}</div>);
+  });
 
-            console.log('this is eachSegment after map', eachSegment);
-            return (
-              <div className="container">
-                {eachSegment}
-                <CommentInput
-                  currentAvatar={props.currentAvatar}
-                  messageId={props.message._id}
-                  to={key}
-                  replyToName={replyToName}
-                />
-              </div>
-            );
-          }
-        }
-      }
-    });
-    eachComment(segment);
+  const CommentsThread = p => (
+    <div>
+      {p.children}
+      <input type="text" />
+    </div>
+    );
+  const xyz = [];
+
+  for (const key in commentBuckets) {
+    xyz.push(<CommentsThread>
+      {commentBuckets[key]}
+    </CommentsThread>
+  );
   }
 
-  const eachComment = props.message.comments.map(comment => (
-    <CommentNode
-      comment={comment}
-      messageId={props.message._id}
-      from={
-            comment.from in props.members
-              ? props.members[comment.from].nickname
-              : '...loading...'
-          }
-      fromAvatar={
-            (comment.from in props.members)
-              ? props.members[comment.from].avatar
-              : null
-          }
-      key={comment._id}
-    />
-    ));
+  console.log('final buckets', commentBuckets);
 
-  const replyToName = props.message.userId in props.members
-      ? props.members[props.message.userId].nickname
-      : '...loading...';
-
-  console.log('this is eachComment for simple comment', eachComment);
   return (
     <div className="container">
-      {eachComment}
-      <CommentInput
-        currentAvatar={props.currentAvatar}
-        messageId={props.message._id}
-        to={props.message.userId}
-        replyToName={replyToName}
-      />
+      {xyz}
     </div>
   );
 };
+
 
 CommentsContainer.defaultProps = {
   members: {},
