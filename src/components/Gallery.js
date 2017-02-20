e49429592ad838dcb891d55417ec834cca5a56f2
alexpoe <alexpoe22@gmail.com>
2017-02-20 14:10:10 -0800

    Gallery filter functionality
    * sorted photos by date in gallery
    
    * filter photos by clicking on user icon
    
    * filter functionality working via family icon click
    
    * date separator completed with basic styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,9 +15,16 @@ class Gallery extends Component {
     this.state = {
       photos: this.props.messages.filter(message =>
         message.contentType === 'photo'
-      )
+      ).sort((a, b) => {
+        const c = new Date(a.date);
+        const d = new Date(b.date);
+        return c - d;
+      }),
+      filterOn: false,
+      filterId: '58a79e7829e48da02c0bb22d',
     };
 
+    this.renderPhotos = this.renderPhotos.bind(this);
     this.openZoom = this.openZoom.bind(this);
     this.closeZoom = this.closeZoom.bind(this);
     this.closeZoom = this.closeZoom.bind(this);
@@ -54,10 +61,71 @@ class Gallery extends Component {
     this.props.dispatch(showZoomed(this.state.photos[newIndex].url, newIndex));
   }
 
+  filterPhotos(id) {
+    this.setState({ filterId: id });
+    if (!this.state.filterOn) {
+      return this.setState({ filterOn: true });
+    }
+    if (id === this.state.filterId) {
+      return this.setState({ filterOn: false });
+    }
+  }
+
+  renderPhotos(photos) {
+    let filteredPhotos = photos;
+    if (this.state.filterOn) {
+      filteredPhotos = filteredPhotos.filter((photo) => {
+        if (this.state.filterId !== photo.userId) {
+          return false;
+        }
+        return photo;
+      }); //
+    }
+
+    let previousMonth = '';
+    const photos123 = [];
+    filteredPhotos.forEach((photo, i) => {
+      const date = new Date(photo.date);
+      if (previousMonth !== `${date.getMonth()}-${date.getYear() + 1900}`) {
+        previousMonth = `${date.getMonth()}-${date.getYear() + 1900}`;
+        photos123.push(<div className="gallerySeparator">{previousMonth}</div>);
+      }
+      photos123.push(
+        // sometimes get a div here, but not wrap it all in a div
+        <GalleryThumbnail
+          key={photo._id}
+          photoIndex={i}
+          photoUrl={photo.url}
+          user={photo.userId}
+          onClick={this.openZoom}
+        />
+      );
+    });
+    return photos123;
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <ul className="userPhotoIcon" style={{ listStyle: 'none' }}>
+          {
+              Object.keys(this.props.members).map(member => (
+                <li
+                  key={this.props.members[member]._id}
+                  onClick={this.filterPhotos.bind(
+                    this, this.props.members[member]._id
+                  )}
+                >
+                  <img
+                    src={this.props.members[member].avatar}
+                    alt="avatar"
+                    style={{ maxWidth: '50px', borderRadius: '50%' }}
+                  />
+                </li>
+              ))
+            }
+        </ul>
         {
           this.props.zoomed ?
             <GalleryZoomed
@@ -70,18 +138,7 @@ class Gallery extends Component {
         }
 
         <div className="galleryContainer">
-          {
-            this.state.photos.map((photo, i) => (
-              <GalleryThumbnail
-                key={photo._id}
-                photoIndex={i}
-                photoUrl={photo.url}
-                user={photo.userId}
-                onClick={this.openZoom}
-              />
-                )
-            )
-          }
+          {this.renderPhotos(this.state.photos)}
         </div>
       </div>
     );
@@ -94,6 +151,7 @@ Gallery.defaultProps = {
 };
 
 Gallery.propTypes = {
+  members: React.PropTypes.objectOf(React.PropTypes.object),
   dispatch: React.PropTypes.func.isRequired,
   messages: React.PropTypes.arrayOf(React.PropTypes.object),
   zoomed: React.PropTypes.bool.isRequired,
