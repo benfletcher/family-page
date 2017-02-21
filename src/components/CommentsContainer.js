49ccd0e41232bf66e590c67a17987def350c0783
Jamie <jamie.davella@gmail.com>
2017-02-20 20:00:53 -0500

    Comments sub conversations functional (#55)
    * create an array for each message with userId of each person who sent a comment and filter out duplicates.
    
    * segment comment data
    
    * continue filtering comment conversations by responder
    
    * implement bucket sort to segment comments then render jsx with input box underneath each

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -3,46 +3,97 @@ import CommentNode from './CommentNode';
 import CommentInput from './CommentInput';
 
 const CommentsContainer = (props) => {
-    // if the current user is the sender of message then Complex comments
-      // where back and forth comments with input field at the end of that subConvo
-    // if (this.props.currentUser === this.props.message.userId) {
-    //
-    // }
-    // else simple Comments where one to one and do not need input field
-  const eachComment = props.message.comments.map(comment => (
-    <CommentNode
-      comment={comment}
-      messageId={props.message._id}
-      from={
-            comment.from in props.members
-              ? props.members[comment.from].nickname
-              : '...loading...'
-          }
-      fromAvatar={
-            (comment.from in props.members)
-              ? props.members[comment.from].avatar
-              : null
-          }
-      key={comment._id}
-    />
-    ));
+  const commentBuckets = {};
 
-  const replyToName = props.message.userId in props.members
-      ? props.members[props.message.userId].nickname
-      : '...loading...';
+  props.message.comments.forEach((comment) => {
+    // console.log(props.currentUser);
+    if (comment.from === props.currentUser) {
+      if (!commentBuckets[comment.to]) { commentBuckets[comment.to] = []; }
+      commentBuckets[comment.to].push(
+        <div>
+          <CommentNode
+            comment={comment}
+            messageId={props.message._id}
+            from={
+          comment.from in props.members
+           ? props.members[comment.from].nickname
+           : '...loading...'
+          }
+            fromAvatar={
+          (comment.from in props.members)
+           ? props.members[comment.from].avatar
+           : null
+          }
+            key={comment._id}
+          />
+        </div>);
+    } else {
+      if (!commentBuckets[comment.from]) { commentBuckets[comment.from] = []; }
+      commentBuckets[comment.from].push(<div>
+        <CommentNode
+          comment={comment}
+          messageId={props.message._id}
+          from={
+     comment.from in props.members
+       ? props.members[comment.from].nickname
+       : '...loading...'
+   }
+          fromAvatar={
+     (comment.from in props.members)
+       ? props.members[comment.from].avatar
+       : null
+   }
+          key={comment._id}
+        />
+      </div>);
+    }
+  });
+
+  const CommentsThread = (p) => {
+    const replyToName = p.to in p.members
+    ? p.members[p.to].nickname
+    : '...loading...';
+
+    console.log('this is props.children inside commentsThread', p.children);
+    return (
+      <div>
+        {p.children}
+        <CommentInput
+          currentAvatar={p.currentAvatar}
+          messageId={p.messageId}
+          to={p.to}
+          replyToName={replyToName}
+        />
+      </div>
+    );
+  };
+
+  const xyz = [];
+  for (const key in commentBuckets) {
+    if (commentBuckets.hasOwnProperty(key)) {
+      xyz.push(
+        <CommentsThread
+          to={key}
+          currentAvatar={props.currentAvatar}
+          messageId={props.message._id}
+          messageUserId={props.message.userId}
+          members={props.members}
+        >
+          {commentBuckets[key]}
+        </CommentsThread>
+      );
+    }
+  }
+
+  console.log('final buckets', commentBuckets);
 
   return (
     <div className="container">
-      {eachComment}
-      <CommentInput
-        currentAvatar={props.currentAvatar}
-        messageId={props.message._id}
-        to={props.message.userId}
-        replyToName={replyToName}
-      />
+      {xyz}
     </div>
   );
 };
+
 
 CommentsContainer.defaultProps = {
   members: {},
