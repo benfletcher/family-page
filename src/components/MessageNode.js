0afba710f9e63578d5002dfe7c05f3fa7b1ac10f
alexpoe22 <alexpoe22@gmail.com>
2017-02-21 13:11:50 -0800

    delete hover

--- src/components/MessageNode.js
+++ src/components/MessageNode.js
@@ -25,7 +25,7 @@ const MessageNode = (props) => {
           props.message.contentType === 'photo'
             ?
               <div>
-                <div className="delete">x</div>
+                <div className="delete"><i className="fa fa-trash-o deleteIcon" aria-hidden="true" /></div>
                 <img className="familyPhoto" src={props.photo} alt="user upload" />
               </div>
             : null
