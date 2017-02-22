1f71570ac586ddc905161e99f6603d8b0976f9f3
alexpoe22 <alexpoe22@gmail.com>
2017-02-22 13:02:29 -0800

    gallery zoomed navigation arrows positioned on desktop

--- src/components/GalleryZoomed.js
+++ src/components/GalleryZoomed.js
@@ -4,21 +4,21 @@ const GalleryZoomed = props => (
   <div className="galleryZoomed">
     <div className="flexZoomedContainer">
       <div className="zoomedPhotoContainer">
+        <div className="zoomedNav">
+          <i
+            className="fa fa-3x white fa-arrow-left arrowLeft"
+            onClick={props.goLeft}
+            aria-hidden="true"
+          />
+          <i
+            className="fa fa-3x white fa-arrow-right arrowRight"
+            onClick={props.goRight}
+            aria-hidden="true"
+          />
+        </div>
         <i onClick={props.zoom} className="xIcon fa fa-times" aria-hidden="true"alt="close" />
         <img className="zoomedPhoto" src={props.photoUrl} alt="large" />
       </div>
-    </div>
-    <div className="zoomedNav">
-      <i
-        className="fa fa-3x white fa-arrow-left"
-        onClick={props.goLeft}
-        aria-hidden="true"
-      />
-      <i
-        className="fa fa-3x white fa-arrow-right"
-        onClick={props.goRight}
-        aria-hidden="true"
-      />
     </div>
   </div>
 );
