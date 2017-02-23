305cf36140ea79e5fbb1c78f2759fac87a16be5f
Jamie <jamie.davella@gmail.com>
2017-02-22 19:27:13 -0500

    Delete functionality on Messages and Comments (#63)
    * add delete dispatch to messageNode component and removed footer from app.js
    
    * remove messageFooter from all messages rendered to the page
    
    * add dispatch to delete comment in comment node component

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import MessageNode from './MessageNode';
 import Header from './Header';
 import CommentsContainer from './CommentsContainer';
 import Announcement from './Announcement';
-import MessageFooter from './MessageFooter';
+import CommentInput from './CommentInput';
 import UserPhotoIcons from './UserPhotoIcons';
 
 export class App extends Component {
@@ -26,7 +26,6 @@ export class App extends Component {
     if (this.props.location.query.token) {
       cookie.save('accessToken', this.props.location.query.token);
     }
-    console.log(this.props.location.query.token);
     this.props.dispatch(fetchMessages());
     this.props.dispatch(fetchMembers());
     this.props.dispatch(fetchCurrentUser());
@@ -48,22 +47,33 @@ export class App extends Component {
               ? this.props.members[message.userId].nickname
               : '...loading...';
 
-            if (message.comments.length === 0) {
+            if ((message.comments.length === 0) && (message.userId === this.props.currentUser)) {
+              return (
+                <div>
+                  <MessageNode
+                    message={message}
+                    currentUser={this.props.currentUser}
+                    memberAvatar={
+                    (message.userId in this.props.members)
+                      ? this.props.members[message.userId].avatar
+                      : null
+                    }
+                  />
+                </div>
+              );
+            } else if (message.comments.length === 0) {
               return (
                 <div key={message._id}>
                   <MessageNode
                     message={message}
-                    commentZoom={this.postComment}
-                    user={message.userId}
-                    photo={message.url}
-                    caption={message.text}
+                    currentUser={this.props.currentUser}
                     memberAvatar={
                       (message.userId in this.props.members)
                         ? this.props.members[message.userId].avatar
                         : null
                       }
                   />
-                  <MessageFooter
+                  <CommentInput
                     currentAvatar={this.props.currentAvatar}
                     messageId={message._id}
                     to={message.userId}
@@ -76,10 +86,7 @@ export class App extends Component {
               <div key={message._id}>
                 <MessageNode
                   message={message}
-                  commentZoom={this.postComment}
-                  user={message.userId}
-                  photo={message.url}
-                  caption={message.text}
+                  currentUser={this.props.currentUser}
                   memberAvatar={
                     (message.userId in this.props.members)
                       ? this.props.members[message.userId].avatar
@@ -92,13 +99,6 @@ export class App extends Component {
                   currentAvatar={this.props.currentAvatar}
                   currentUser={this.props.currentUser}
                   members={this.props.members}
-                />
-
-                <MessageFooter
-                  currentAvatar={this.props.currentAvatar}
-                  messageId={message._id}
-                  to={message.userId}
-                  replyToName={replyToName}
                 />
               </div>
             );
