4ccc3a213f36d41f244ec96dd94aed2f36403927
davellaj <jamie.davella@gmail.com>
2017-03-03 20:43:15 -0500

    add create family avatar and made initial style familyChoice page with css from other components

--- src/components/FamilyChoice.js
+++ src/components/FamilyChoice.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
+import { hashHistory, Link } from 'react-router';
 import { fetchCurrentUser } from '../actions/current-user';
 import { switchFamily } from '../actions/family';
 
+
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 class FamilyChoice extends Component {
   constructor(props) {
     super(props);
 
     this.selectFamily = this.selectFamily.bind(this);
-    this.familyInput = this.familyInput.bind(this);
+    this.createFamily = this.createFamily.bind(this);
   }
   componentDidMount() {
     this.props.dispatch(fetchCurrentUser());
@@ -21,41 +23,58 @@ class FamilyChoice extends Component {
     hashHistory.push('/app');
   }
 
-  familyInput(event) {
-    event.preventDefault();
+  createFamily() {
     console.log('ready for form popout');
   }
 
   render() {
     return (
       <div>
-        <div className="familiesContainer" style={{ listStyle: 'none' }}>
+        <div className="navButton" >
+          <p className="navTitle">
+            <a href={`${serverUrl}/auth/logout`}>
+              Logout
+            </a>
+          </p>
+        </div>
+        <div className="familiesContainer" >
           {
-      this.props.families.map(family => (
-        <div
-          className="familyContainer"
-          key={family._id}
-          onClick={() => this.selectFamily(family._id)}
-        >
+            this.props.families.map(family => (
+              <div
+                className="familyContainer"
+                key={family._id}
+                onClick={() => this.selectFamily(family._id)}
+              >
+                <img
+                  src={family.avatar}
+                  alt="avatar"
+                  style={{ maxWidth: '5%', borderRadius: '50%' }}
+                />
+                <div className="familyNamesContainer">
+                  <div>
+                    {family.name}
+                  </div>
+                  <div>
+                    {family.members.map(member => <span key={member._id}>{`${member.nickname}, `}</span>)
+                  }
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <div className="familyContainer">
           <img
-            src={family.avatar}
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSS7tLRutVL3cSVqtulqbDXwVdmpD3MCyJh2k2zWVogovBy1nC_"
             alt="avatar"
-            style={{ maxWidth: '10%', borderRadius: '50%' }}
+            style={{ maxWidth: '5%', borderRadius: '50%' }}
           />
           <div className="familyNamesContainer">
-            <div>
-              {family.name}
-            </div>
-            <div>
-              {family.members.map(member => <span>{`${member.nickname}, `}</span>)
-            }
+            <div onClick={this.createFamily}>
+              Create a New Family
             </div>
           </div>
         </div>
-      ))
-    }
-        </div>
-        <button onClick={this.familyInput}>Create a Family</button>
       </div>
     );
   }
@@ -65,7 +84,7 @@ FamilyChoice.defaultProps = {
 };
 
 FamilyChoice.propTypes = {
-  families: React.PropTypes.object.isRequired,
+  families: React.PropTypes.array.isRequired,
   dispatch: React.PropTypes.func.isRequired,
 };
 
